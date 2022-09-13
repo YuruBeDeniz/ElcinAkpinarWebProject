19f99fe66bd84fb0248d2295aae1661f0bc91325
Antonio Saleme <79271990+thusspokedata@users.noreply.github.com>
2022-09-13 10:44:32 +0200

    fix app.js

--- client/src/App.js
+++ client/src/App.js
@@ -4,15 +4,16 @@ import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import { Footer } from "./components";
 import Home from "./pages/Home";
+import AboutMe from "./pages/AboutMe";
 
 export default function App() {
   return (
-    <div className="text-xl font-bold">
+    <div>
       <div>
-        <Navbar />
-
+        <Navbar className="text-xl font-bold" />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/aboutme" element={<AboutMe />} />
         </Routes>
         <Footer />
       </div>
