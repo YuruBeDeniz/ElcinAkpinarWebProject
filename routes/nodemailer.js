3baac5ff58a8ca226ef06123667aef5884828901
Deniz Saganak <saganak.deniz@gmail.com>
2022-09-25 11:22:41 +0200

    add contact page backend

--- routes/nodemailer.js
+++ routes/nodemailer.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const nodemailer = require("nodemailer");
 require("dotenv/config");
 
-router.post('/send-email', (res, req, next) => {
+router.post('/send-email', (req, res, next) => {
     let { name, email, subject, message} = req.body;
     let transporter = nodemailer.createTransport({
         service: 'Gmail',
@@ -13,16 +13,14 @@ router.post('/send-email', (res, req, next) => {
       });
     transporter
     .sendMail({
-      from: '"Elcin Akpinar Website"',
+      from: "Elcin Akpinar Website",
       to: process.env.TOEMAIL,
       name: name,
       subject: subject,
       text: message,
       html: `You recived a message: <b>${message}</b> from ${email}`,
     })
-    .then(sentEmail => {
-        const {name, email, subject, message } = sentEmail;
-        res.json("message", sentEmail)})
+    .then(info => console.log(info))
     .catch((error) => console.log("Error Occurs!"));  
 })
 
