180fa029debc80ff6600dc8d56d7887ef728bc26
Deniz Saganak <saganak.deniz@gmail.com>
2023-02-27 09:04:09 +0100

    fix soundcloud icon

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -5,7 +5,7 @@ export default function Navbar({ fixed }) {
   const [navbarOpen, setNavbarOpen] = React.useState(false);
   return (
     <>
-      <nav className="relative flex items-center justify-between px-2 py-3 w-100 left-0 right-0 bg-slate-900 mb-3">
+      <nav className="relative right-0 left-0 flex items-center justify-between px-2 py-3 bg-slate-900 mb-3">
         <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
           <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
               <Link className="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-white" to='/'>Home</Link>
